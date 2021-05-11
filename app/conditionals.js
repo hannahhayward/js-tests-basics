60ae8b6ece78cc731cc092d895f1ff9778695c25
hannah hayward <hannahhayward97@gmail.com>
2021-05-11 09:41:39 -0600

    first try

--- app/conditionals.js
+++ app/conditionals.js
@@ -1,8 +1,12 @@
 //1. Given two numbers, write a function that will return  the larger number
 
 function largerNum(num1, num2) {
-    // you code here
-
+    if (num1>num2) {
+         return (num1)
+        
+    } else {
+        return (num2)
+    }
 }
 
 // 2. Given two numbers, amount correct and amount possible of a test, return the corresponding letter grade.
@@ -14,7 +18,7 @@ function largerNum(num1, num2) {
 // output: "F"
 
 function testGrader(score, possible) {
-
+    return 
 }
 
 
